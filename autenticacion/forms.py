fda53ebc13428a034e512a210fda83d77b804049
Unknown <pedro.ignaciom95@gmail.com>
2018-05-21 23:21:04 -0400

    feat: formulario basico para pruebas

---
+++ autenticacion/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import Usuario
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = Usuario
+        fields = ('username', 'email')
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = Usuario
+        fields = UserChangeForm.Meta.fields
