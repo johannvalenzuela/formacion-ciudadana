aa4fe6c37522196d3054851552dc4284638c4b91
Unknown <pedro.ignaciom95@gmail.com>
2018-05-21 23:03:04 -0400

    feat: Creacion de la app de autenticacion

--- formacionciudadana/settings.py
+++ formacionciudadana/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'autenticacion.apps.AutenticacionConfig'
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-AUTH_USER_MODEL = 'autenticacion.usuario'
+AUTH_USER_MODEL = 'autenticacion.Usuario'
