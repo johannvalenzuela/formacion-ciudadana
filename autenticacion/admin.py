1564b0b0758eadef7467530817abc39e0e84e273
Unknown <pedro.ignaciom95@gmail.com>
2018-05-21 23:22:33 -0400

    feat: modificacion del admin
    se agrega el customAdmin

--- autenticacion/admin.py
+++ autenticacion/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import Usuario
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = Usuario
+    list_display = ['email', 'username', 'name']
+
+admin.site.register(Usuario, CustomUserAdmin)
