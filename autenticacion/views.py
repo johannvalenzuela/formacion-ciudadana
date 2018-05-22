900aa72d02ba7896fad038c602dcd8d9a36d5426
Unknown <pedro.ignaciom95@gmail.com>
2018-05-22 00:12:28 -0400

    feat:creacion de entorno de prueba

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -6,4 +6,4 @@ from .forms import CustomUserCreationForm
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/signup.html'
