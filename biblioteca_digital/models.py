9b712fcdaf3209aa0d9db8e45d812d1da7bab104
Unknown <pedro.ignaciom95@gmail.com>
2018-05-23 23:09:17 -0400

    feat: Ingreso de modelo de recurso
    Se ingreso el modelo de recursos que estaba en la rama development

--- biblioteca_digital/models.py
+++ biblioteca_digital/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Recurso(models.Model):
+    titulo = models.CharField(max_length = 255)
+    descripcion = models.CharField(max_length = 255)
+    valoracion = models.CharField(max_length = 255)
+    fecha_creacion = models.DateField("%d-%m-%Y")
+    asignatura = models.ForeignKey('Asignatura', on_delete = models.CASCADE)
