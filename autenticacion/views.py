28e595742c76ebbbd0f7cf81192648da4d2a6ff2
Unknown <pedro.ignaciom95@gmail.com>
2018-05-23 20:15:27 -0400

    feat: agregado view de politicas de privacidad

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,9 +1,12 @@
 from django.urls import reverse_lazy
 from django.views import generic
-
+from django.views.generic import TemplateView
 from .forms import CustomUserCreationForm
 
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class PoliticaPrivacidad(TemplateView):
+    template_name = 'registration/politica_privacidad.html'
