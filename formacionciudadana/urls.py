a83d8f2815e6ebb9951aea3c2cdf2a533e8fdce5
Unknown <pedro.ignaciom95@gmail.com>
2018-05-23 20:15:56 -0400

    feat: Agregado url de politica de privacidad

--- formacionciudadana/urls.py
+++ formacionciudadana/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('autenticacion/', include('autenticacion.urls')),
-    path('autenticacion/', include('django.contrib.auth.urls')),
+    
 ]
