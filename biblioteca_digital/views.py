24111d0aa41a73c5f1669c7dc192a4d3fb84fccd
Unknown <pedro.ignaciom95@gmail.com>
2018-05-25 00:00:03 -0400

    fix: Cambio de extension de view de bibliotecaView
    Se cambio de TemplateView a ListView

--- biblioteca_digital/views.py
+++ biblioteca_digital/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from .models import Recurso
+from django.views import generic
 
-class BibliotecaView(TemplateView):
+class BibliotecaView(generic.ListView):
     template_name = 'biblioteca_digital/principal.html'
-    
+    context_object_name = 'lista_recursos'
+    form_class = 'BiblitecaForm'
+
+    def get_queryset(self):
+        """Retorna los 5 recursos mejor valorados."""
+        return Recurso.objects.order_by('valoracion')[:5]
