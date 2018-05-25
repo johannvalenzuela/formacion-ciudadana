3f548d1709be45c0d0cf8373cc30271732577379
Unknown <pedro.ignaciom95@gmail.com>
2018-05-24 20:58:17 -0400

    feat: Agreado del view principal de la app

--- biblioteca_digital/views.py
+++ biblioteca_digital/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class BibliotecaView(TemplateView):
+    template_name = 'biblioteca_digital/principal.html'
+    
