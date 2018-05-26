a5aaf2392aa2f185a0dbf6dc000f42109cd5b4ea
Unknown <pedro.ignaciom95@gmail.com>
2018-05-25 21:32:45 -0400

    feat: Se agrega la vista y el template consulta
    se agrega la vista y el template de la visualización de las consultas.

--- consulta/views.py
+++ consulta/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Consulta
 
 # Create your views here.
+class visualizarConsultas(generic.ListView):
+    template_name = 'consulta/visualizar_consultas.html'
+    context_object_name = 'lista_consultas'
+
+    def get_queryset(self):
+        """Retorna las consultas."""
+        return Consulta.objects.all()
