0cf8b265ccf49f77e15e1f1e7781685c036bb304
Unknown <pedro.ignaciom95@gmail.com>
2018-05-26 19:23:53 -0400

    fix: Nombre de view consulta mal escrito

--- consulta/urls.py
+++ consulta/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.VisualizarConsultas.as_view(), name='visualizar_consultas'),
+    path('', views.VisualizarConsultasView.as_view(), name='visualizar_consultas'),
 ]
