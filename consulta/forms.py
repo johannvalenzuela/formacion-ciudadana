d5994ed926d808454f2e9e85984f668dadaa945e
Unknown <pedro.ignaciom95@gmail.com>
2018-05-26 19:32:56 -0400

    feat: se agrega formulario para propuesta de consulta

--- consulta/forms.py
+++ consulta/forms.py
@@ -1,16 +1,28 @@
 from django import forms
-from .models import Consulta
+from .models import Consulta, Consulta_propuesta, Consulta_respuesta
 from datetime import datetime
 
-class consultaForm(forms.ModelForm):
+class ConsultaForm(forms.ModelForm):
     '''
     Formulario para la creacion de la consulta
     '''
     titulo = forms.CharField()
     fecha_finalizacion = models.DateField(widget=SelectDateWidget, initial=datetime.now) 
     fecha_inicio = models.DateField(widget=SelectDateWidget, initial=datetime.now) 
-    descripcion = forms.CharField(widget= froms.Textarea) 
+    descripcion = forms.CharField(widget= forms.Textarea) 
 
     class Meta:
             model = Consulta
             fields = ('titulo', 'descripcion', 'fecha_inicio', 'fecha_finalizacion')
+
+
+class ConsultaPropuestaForm(forms.Form):
+    '''
+    Formulario para crear alternativas para una consulta
+    '''
+    titulo = forms.CharField()
+    descripcion = forms.CharField(widget= forms.Textarea) 
+    contenido = models.TextField(widget= forms.Textarea) 
+    class Meta:
+            model = Consulta_propuesta
+            fields = ('titulo', 'descripcion', 'contenido')
