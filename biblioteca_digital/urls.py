f4633f762987b76a6dbaab309ad04c9a6914f602
Unknown <pedro.ignaciom95@gmail.com>
2018-05-27 23:18:23 -0400

    feat: Agregado camino recurso a las url

--- biblioteca_digital/urls.py
+++ biblioteca_digital/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.BibliotecaView.as_view(), name='principal'),
+    path('recurso/', views.RecursoView.as_view(), name='recurso'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
