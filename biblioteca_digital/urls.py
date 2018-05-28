cf448128b9358e6226801876c0d02b97f921d473
Unknown <ogasvega.israel@gmail.com>
2018-05-27 21:55:04 -0400

    feat: añadida ruta para detalle de un recurso

--- biblioteca_digital/urls.py
+++ biblioteca_digital/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.BibliotecaView.as_view(), name='principal'),
+    path('<pk>/', views.RecursoDetailView.as_view(), name='recurso-detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
