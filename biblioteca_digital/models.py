d1805927f57630cb628ad2e682c315e8f271b97d
Unknown <pedro.ignaciom95@gmail.com>
2018-05-29 01:31:28 -0400

    fix: corregido de llamada a la funcion equivocada

--- biblioteca_digital/models.py
+++ biblioteca_digital/models.py
@@ -24,7 +24,7 @@ def update_filename(instance, filename):
 class Recurso(models.Model):
     titulo = models.CharField(max_length = 255, blank = False, null = False, unique=True)
     descripcion = models.CharField(max_length = 255, blank = False, null = False)
-    imagen_descriptiva = models.FileField(upload_to=update_filename, null = False)
+    imagen_descriptiva = models.FileField(upload_to=update_imagen, null = False)
     fecha_creacion = models.DateTimeField(default=datetime.now)
     tema = models.PositiveSmallIntegerField(choices=TEMA_ALTERNATIVAS,default=1)
     archivo = models.FileField(upload_to=update_filename, null = False)
