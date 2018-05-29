abf3b08a4241a7f18f3279d0470440fe644bd35e
Unknown <pedro.ignaciom95@gmail.com>
2018-05-29 04:56:16 -0400

    feat: usuario admin modificado

--- autenticacion/admin.py
+++ autenticacion/admin.py
@@ -1,4 +1,28 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Usuario
+from .forms import UserChangeForm , UserCreationForm
 
-admin.site.register(Usuario)
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'is_superuser', 'is_staff', 'is_active',)
+    # list_filter = ('email',)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    # filter_horizontal = ()
+
+admin.site.register(Usuario, UserAdmin)
