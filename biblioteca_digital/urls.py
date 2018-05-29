baa3f56fb0f2d5d1639c3350dee9f52607b983d0
Unknown <pedro.ignaciom95@gmail.com>
2018-05-29 01:30:42 -0400

    feat: se agrega url de descarga

--- biblioteca_digital/urls.py
+++ biblioteca_digital/urls.py
@@ -5,11 +5,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.BibliotecaView.as_view(), name='principal'),
+    path('', views.BibliotecaView.as_view()),
     path('<pk>/', views.RecursoDetailView.as_view(), name='recurso-detail'),
     path('<pk>/', views.RecursoDetailView.valorar, name='valorar'), 
+    path('<pk>/descargar/', views.descargar, name= 'descargar'),
 ]
 
 if  settings.DEBUG:
-     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
