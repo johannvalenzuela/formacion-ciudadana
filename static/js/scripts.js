bb16a6c2539fe3640b711c6cdf26c876dcde7f71
Unknown <ogasvega.israel@gmail.com>
2018-05-29 02:11:45 -0400

    feat: añadido script para clickear estrellas

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,21 +1,26 @@
- window.onscroll = () => {
+window.onscroll = () => {
     const nav = document.querySelector('#navBarSection');
     if (nav) {
         if (this.scrollY <= 50) nav.className = 'navbar fixed-top navbar-expand-lg navbar-transparent'; else nav.className = 'navbar navbar-expand-lg sticky-top navbar-light bg-light py-2 shadow';
     }
-}; 
+};
 
 var $star_rating = $('.star-rating .mi');
 
-var SetRatingStar = function() {
-  return $star_rating.each(function() {
-    if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
-      return $(this).removeClass('mi-FavoriteStar').addClass('mi-FavoriteStarFill');
-    } else {
-      return $(this).removeClass('mi-FavoriteStarFill').addClass('mi-FavoriteStar');
-    }
-  });
+var SetRatingStar = function () {
+    return $star_rating.each(function () {
+        if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+            return $(this).removeClass('mi-FavoriteStar').addClass('mi-FavoriteStarFill');
+        } else {
+            return $(this).removeClass('mi-FavoriteStarFill').addClass('mi-FavoriteStar');
+        }
+    });
 };
+
+$star_rating.on('click', function () {
+    $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+    return SetRatingStar();
+});
 
 SetRatingStar();
 
