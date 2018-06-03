fc89178570c7dbfb40e25be5fc748d6c9123fce4
Unknown <ogasvega.israel@gmail.com>
2018-06-03 03:14:47 -0400

    feat: añadido script para abrir modal de editar recurso

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -68,3 +68,27 @@ function valorEstrella(estrella, token){
 
 }
 
+// Script para mostrar modal de editar recurso
+$(document).ready(function () {
+
+    $(".contact").click(function (ev) { // for each edit contact url
+        ev.preventDefault(); // prevent navigation
+        var url = $(this).data("form"); // get the contact form url
+        console.log(url);
+        $("#recursoModal").load(url, function () { // load the url into the modal
+            $(this).modal('show'); // display the modal on url load
+        });
+    });
+
+    $('.recurso-form').on('submit', function () {
+        $.ajax({
+            type: $(this).attr('method'),
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            context: this,
+            success: function (data, status) {
+                $('#recursoModal').html(data);
+            }
+        });
+    });
+});
