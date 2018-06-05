dbda163153900907b105e81706e7d485bac00f25
Unknown <pedro.ignaciom95@gmail.com>
2018-06-05 19:56:34 -0400

    feat: Script para mostrar el editar y eliminar a través de un modal

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -92,3 +92,28 @@ $(document).ready(function () {
         });
     });
 });
+
+// Script para mostrar modal de editar o eliminar comentarioRecurso
+$(document).ready(function () {
+    
+    $(".comentarioRecurso").click(function (ev) { // for each edit contact url
+        ev.preventDefault(); // prevent navigation
+        var url = $(this).data("form"); // get the contact form url
+        console.log(url);
+        $("#comentarioRecursoModal").load(url, function () { // load the url into the modal
+            $(this).modal('show'); // display the modal on url load
+        });
+    });
+
+    $('.comentarioRecurso-form').on('submit', function () {
+        $.ajax({
+            type: $(this).attr('method'),
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            context: this,
+            success: function (data, status) {
+                $('#comentarioRecursoModal').html(data);
+            }
+        });
+    });
+});
