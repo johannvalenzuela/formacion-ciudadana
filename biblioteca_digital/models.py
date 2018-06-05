ebd050dffbd00091840a6587bbb529ec111112e9
Unknown <pedro.ignaciom95@gmail.com>
2018-06-04 22:18:54 -0400

    feat: Separado las imagenes de los recursos por carpeta

--- biblioteca_digital/models.py
+++ biblioteca_digital/models.py
@@ -9,25 +9,25 @@ TEMA_ALTERNATIVAS = (
     (3, 'otros'),
 )
 
-def update_imagen(instance, filename):
-    upload_to = 'recursos/'
-    ext = filename.split('.')[-1]
-    filename = '{}_{}.{}'.format(instance.titulo,'img_des', ext)
-    return os.path.join(upload_to, filename)
+# def update_imagen(instance, filename):
+#     upload_to = 'imagenes/'
+#     ext = filename.split('.')[-1]
+#     filename = '{}_{}.{}'.format(instance.titulo,'img_des', ext)
+#     return os.path.join(upload_to, filename)
 
-def update_filename(instance, filename):
-    upload_to = 'recursos/'
-    ext = filename.split('.')[-1]
-    filename = '{}.{}'.format(instance.titulo, ext)
-    return os.path.join(upload_to, filename)
+# def update_filename(instance, filename):
+#     upload_to = 'recursos/'
+#     ext = filename.split('.')[-1]
+#     filename = '{}.{}'.format(instance.titulo, ext)
+#     return os.path.join(upload_to, filename)
 
 class Recurso(models.Model):
     titulo = models.CharField(max_length = 255, blank = False, null = False, unique=True)
     descripcion = models.CharField(max_length = 255, blank = False, null = False)
-    imagen_descriptiva = models.FileField(upload_to=update_imagen, null = False)
+    imagen_descriptiva = models.FileField(upload_to='imagenes/', null = False)
     fecha_creacion = models.DateTimeField(default=datetime.now)
     tema = models.PositiveSmallIntegerField(choices=TEMA_ALTERNATIVAS,default=1)
-    archivo = models.FileField(upload_to=update_filename, null = False)
+    archivo = models.FileField(upload_to='recursos/', null = False)
     valoracionTotal = models.FloatField(default=0)
     cant_valoracion = models.PositiveIntegerField(default=0)
     autor = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
@@ -36,7 +36,6 @@ class Recurso(models.Model):
         valoracionNueva = (int)(valoracionNueva)
         self.cant_valoracion +=1
         self.valoracionTotal = (self.valoracionTotal*(self.cant_valoracion-1) + valoracionNueva)/self.cant_valoracion
-
 
 
     def __str__(self):
