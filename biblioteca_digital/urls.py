99092f06ec18ff1dd9679d54cbfcbee2c506c4f7
Unknown <pedro.ignaciom95@gmail.com>
2018-06-05 04:05:50 -0400

    feat: se agrega la url para comentar

--- biblioteca_digital/urls.py
+++ biblioteca_digital/urls.py
@@ -8,10 +8,12 @@ from autenticacion.decorators import funcionario_required
 
 urlpatterns = [
     path('', views.BibliotecaView.as_view(), name='biblioteca_digital'),
+
     path('<int:pk>/', views.RecursoDetailView.as_view(), name='recurso-detail'),
+    path('<int:pk>/valorar/', views.RecursoDetailView.valorar, name='valorar'), 
+    path('<int:pk>/comentar', views.RecursoDetailView.comentar, name='comentar'),
     path('<int:pk>/editar', views.RecursoUpdateView.as_view(), name='recurso-update'),
     path('<int:pk>/eliminar', views.RecursoDeleteView.as_view(), name='recurso-delete'),
-    path('<int:pk>/valorar/', views.RecursoDetailView.valorar, name='valorar'), 
     path('<int:pk>/descargar/', views.descargar, name= 'descargar'),
     path('recurso_nuevo/', views.CrearRecursoView.as_view(), name='recurso_nuevo'),
 ]
