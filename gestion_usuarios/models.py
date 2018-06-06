f05665d22f859166eb016f468f479cf790a6f440
Unknown <pedro.ignaciom95@gmail.com>
2018-06-06 18:09:22 -0400

    feat: Agregado los modelos de gestion de usuarios

--- gestion_usuarios/models.py
+++ gestion_usuarios/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Servicio_Local_Educacion(models.Model):
+    nombre = models.CharField(max_lenght = 255)
+    def __str__(self):
+        return (self.nombre)
+
+class Departamento_Provincial_Educacion(models.Model):
+    nombre = models.CharField(max_lenght = 255)
+    def __str__(self):
+        return (self.nombre)
+
+class Establecimiento(models.Model):
+    nombre = models.CharField(max_lenght = 255)
+    direccion = models.CharField(max_lenght = 255)
+    departamento_Provincial_Educacion = models.ForeignKey('Departamento_Provincial_Educacion', on_delete = models.CASCADE)
+    servicio_Local_Educacion = models.ForeignKey('Servicio_Local_Educacion', on_delete = models.CASCADE)
+    def __str__(self):
+        return (self.nombre)
+
+class Asignatura(models.Model):
+    nombre = models.CharField(max_lenght = 255)
+    def __str__(self):
+        return (self.nombre)
+
+class Encargado(models.Model):
+    establecimiento = models.ForeignKey('Establecimiento', on_delete = models.CASCADE)
+    asignatura = models.ForeignKey('Asignatura', on_delete = models.CASCADE)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    
+class Supervisor(models.Model):
+    asignatura = models.ForeignKey('Asignatura', on_delete = models.CASCADE)
+    departamento_Provincial_Educacion = models.ForeignKey('Departamento_Provincial_Educacion', on_delete = models.CASCADE)
+    servicio_Local_Educacion = models.ForeignKey('Servicio_Local_Educacion', on_delete = models.CASCADE)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+class Grupo(models.Model):
+    nombre = models.CharField(max_lenght = 255)
+    establecimiento = models.ForeignKey('Establecimiento', on_delete = models.CASCADE)
+    usuarios = models.ManyToManyField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    def __str__(self):
+        return (self.nombre)
+
+
