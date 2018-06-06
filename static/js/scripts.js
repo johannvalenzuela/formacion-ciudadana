fae4bd9e49e5f8895a8df4049c987e1c6a24b66d
Unknown <by.darklancer@gmail.com>
2018-06-06 14:00:06 -0400

    feat: actualizacion de la carpeta static

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,29 +1,49 @@
- window.onscroll = () => {
+var valorado = false;
+window.onscroll = () => {
     const nav = document.querySelector('#navBarSection');
     if (nav) {
         if (this.scrollY <= 50) nav.className = 'navbar fixed-top navbar-expand-lg navbar-transparent'; else nav.className = 'navbar navbar-expand-lg sticky-top navbar-light bg-light py-2 shadow';
     }
-}; 
-
-var $star_rating = $('.star-rating .mi');
-
-var SetRatingStar = function() {
-  return $star_rating.each(function() {
-    if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
-      return $(this).removeClass('mi-FavoriteStar').addClass('mi-FavoriteStarFill');
-    } else {
-      return $(this).removeClass('mi-FavoriteStarFill').addClass('mi-FavoriteStar');
-    }
-  });
 };
 
+var $total_star_rating = $('.total-star-rating .mi');
+var $star_rating = $('.star-rating .mi');
+
+var showRatingStar = function () {
+    if(!valorado){
+        return $total_star_rating.each(function () {
+            if (parseInt($total_star_rating.siblings('input.total-rating-value').val()) >= parseInt($(this).data('rating'))) {
+                return $(this).removeClass('mi-FavoriteStar').addClass('mi-FavoriteStarFill');
+            } else {
+                return $(this).removeClass('mi-FavoriteStarFill').addClass('mi-FavoriteStar');
+            }
+        });
+    }
+};
+
+var SetRatingStar = function () {
+    if(!valorado){
+        return $star_rating.each(function () {
+            if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+                return $(this).removeClass('mi-FavoriteStar').addClass('mi-FavoriteStarFill');
+            } else {
+                return $(this).removeClass('mi-FavoriteStarFill').addClass('mi-FavoriteStar');
+            }
+        });
+    }
+};
+
+$star_rating.on('click', function () {
+    if(!valorado){
+        $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+        return SetRatingStar();
+    }
+});
+
+showRatingStar();
 SetRatingStar();
 
 $(document).ready(function () {
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
-
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar, #content').toggleClass('active');
         $('.collapse.in').toggleClass('in');
@@ -37,4 +57,47 @@ $('#sidebarCollapse').on('click', function () {
     } else if ($('#show-panel').css('display') != 'none') {
         $('#hide-panel').show().siblings('span').hide();
     }
+});
+
+
+function valorEstrella(estrella, token){
+    if(!valorado){
+        var valorEstrella = estrella.getAttribute("data-rating");
+        $.ajax({
+            url: 'valorar/',
+            type: "POST",
+            data: {valoracion: valorEstrella,
+                'csrfmiddlewaretoken': token
+            },
+            success:function(m){
+            alert('Gracias por tu valoración');
+            valorado=true;
+            },
+        });
+    }
+}
+
+// Script para mostrar modal de editar/eliminar recursos o comentarios
+$(document).ready(function () {
+
+    $(".recurso").click(function (ev) { // for each edit contact url
+        ev.preventDefault(); // prevent navigation
+        var url = $(this).data("form"); // get the contact form url
+        console.log(url);
+        $("#recursoModal").load(url, function () { // load the url into the modal
+            $(this).modal('show'); // display the modal on url load
+        });
+    });
+
+    $('.recurso-form').on('submit', function () {
+        $.ajax({
+            type: $(this).attr('method'),
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            context: this,
+            success: function (data, status) {
+                $('#recursoModal').html(data);
+            }
+        });
+    });
 });
