5f69aa7b21b5e8116aae69d26b638ddb4358fdd0
Unknown <pedro.ignaciom95@gmail.com>
2018-06-07 18:12:25 -0400

    feat: Cambiado los datos que puede cambiar el admin de usuario

--- autenticacion/admin.py
+++ autenticacion/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Usuario
 
-admin.site.register(Usuario)
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido_paterno', 'apellido_materno', 'rut', 'tipo')
+    #list_filter = ('created', 'updated', 'status')
+    search_fields = ('nombre', 'apellido_paterno', 'apellido_materno', 'rut', 'tipo')
+    fieldsets = [
+        ('Cambiar tipo de usuario', {'fields': ['tipo']}),
+        ('Banear usuario', {'fields': ['is_active']}),
+    ]
+
+
+admin.site.register(Usuario, UsuarioAdmin)
+
--- gestion_usuarios/admin.py
+++ gestion_usuarios/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Servicio_Local_Educacion,Departamento_Provincial_Educacion,Establecimiento,Asignatura
+from .models import Servicio_Local_Educacion
+from .models import Departamento_Provincial_Educacion
+from .models import Establecimiento
+from .models import Asignatura
+from .models import Encargado
+
 
 admin.site.register(Servicio_Local_Educacion)
 admin.site.register(Departamento_Provincial_Educacion)
 admin.site.register(Establecimiento)
 admin.site.register(Asignatura)
-
+admin.site.register(Encargado)
