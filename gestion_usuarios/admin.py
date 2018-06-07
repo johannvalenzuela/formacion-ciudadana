bc6cdb90014bd1ea5702fcc196947ee5e0d08991
Unknown <pedro.ignaciom95@gmail.com>
2018-06-07 15:52:20 -0400

    feat: Agregado los campos de gestion de usuarios al admin

--- gestion_usuarios/admin.py
+++ gestion_usuarios/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Servicio_Local_Educacion,Departamento_Provincial_Educacion,Establecimiento,Asignatura
 
-# Register your models here.
+admin.site.register(Servicio_Local_Educacion)
+admin.site.register(Departamento_Provincial_Educacion)
+admin.site.register(Establecimiento)
+admin.site.register(Asignatura)
+
