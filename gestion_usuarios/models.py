38e0a680e25ef967fce7aa668616e42464be7332
Unknown <pedro.ignaciom95@gmail.com>
2018-06-07 02:17:35 -0400

    fix: Se cambia los atributos que ocacionaban bugs

--- gestion_usuarios/models.py
+++ gestion_usuarios/models.py
@@ -1,26 +1,29 @@
 from django.db import models
-
+from datetime import datetime
+import os
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 class Servicio_Local_Educacion(models.Model):
-    nombre = models.CharField(max_lenght = 255)
+    nombre = models.CharField(max_length = 50)
     def __str__(self):
         return (self.nombre)
 
 class Departamento_Provincial_Educacion(models.Model):
-    nombre = models.CharField(max_lenght = 255)
+    nombre = models.CharField(max_length = 50)
     def __str__(self):
         return (self.nombre)
 
 class Establecimiento(models.Model):
-    nombre = models.CharField(max_lenght = 255)
-    direccion = models.CharField(max_lenght = 255)
+    nombre = models.CharField(max_length = 50)
+    direccion = models.CharField(max_length = 100)
     departamento_Provincial_Educacion = models.ForeignKey('Departamento_Provincial_Educacion', on_delete = models.CASCADE)
     servicio_Local_Educacion = models.ForeignKey('Servicio_Local_Educacion', on_delete = models.CASCADE)
     def __str__(self):
         return (self.nombre)
 
 class Asignatura(models.Model):
-    nombre = models.CharField(max_lenght = 255)
+    nombre = models.CharField(max_length = 50)
     def __str__(self):
         return (self.nombre)
 
@@ -29,6 +32,7 @@ class Encargado(models.Model):
     asignatura = models.ForeignKey('Asignatura', on_delete = models.CASCADE)
     usuario = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     
+    
 class Supervisor(models.Model):
     asignatura = models.ForeignKey('Asignatura', on_delete = models.CASCADE)
     departamento_Provincial_Educacion = models.ForeignKey('Departamento_Provincial_Educacion', on_delete = models.CASCADE)
@@ -36,10 +40,15 @@ class Supervisor(models.Model):
     usuario = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
 
 class Grupo(models.Model):
-    nombre = models.CharField(max_lenght = 255)
+    nombre = models.CharField(max_length = 50)
+    autor_rut = models.CharField(max_length = 15)
     establecimiento = models.ForeignKey('Establecimiento', on_delete = models.CASCADE)
-    usuarios = models.ManyToManyField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
     def __str__(self):
         return (self.nombre)
+
+    class Meta: 
+        verbose_name = _('grupo') 
+        verbose_name_plural = _('grupos') 
 
 
