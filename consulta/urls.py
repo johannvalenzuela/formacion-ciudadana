09eb2c2664653ce50b3e85ee256f8ec422641846
Unknown <by.darklancer@gmail.com>
2018-06-07 13:57:57 -0400

    feat: uso de pk en url detalles_consulta

--- consulta/urls.py
+++ consulta/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.VisualizarConsultasView.as_view(), name='visualizar_consultas'),
-    path('detallesconsulta/', views.DetallesConsultaView.as_view(), name='detalles_consulta'),
+    path('<int:pk>/', views.DetallesConsultaView.as_view(), name='detalles_consulta'),
     path('crearconsulta/', views.CrearConsultaView.as_view(), name = 'crear_consulta'),
     path('detallesconsulta/crearpropuestaconsulta/', views.ConsultaCrearPropuestaView.as_view(), name ='crear_propuesta'),
     path('detallesconsulta/responderconsulta/', views.ResponderConsultaView.as_view(), name= 'responder_consulta'),
