56f7957b37dd2e4a853a9ce44cc3bfe0a0067e85
Unknown <pedro.ignaciom95@gmail.com>
2018-06-07 20:51:10 -0400

    feat: Agregado el supervisor al sistema

--- gestion_usuarios/admin.py
+++ gestion_usuarios/admin.py
@@ -4,6 +4,7 @@ from .models import Departamento_Provincial_Educacion
 from .models import Establecimiento
 from .models import Asignatura
 from .models import Encargado
+from .models import Supervisor
 
 
 admin.site.register(Servicio_Local_Educacion)
@@ -12,9 +13,15 @@ admin.site.register(Establecimiento)
 admin.site.register(Asignatura)
 
 
+
 class EncargadoAdmin(admin.ModelAdmin):
     list_display = ('usuario','asignatura', 'establecimiento')
     search_fields = ('usuario','asignatura', 'establecimiento')
+
+class SupervisorAdmin(admin.ModelAdmin):
+    list_display = ('usuario','asignatura', 'departamento_Provincial_Educacion', 'servicio_Local_Educacion')
+    search_fields = ('usuario','asignatura', 'departamento_Provincial_Educacion', 'servicio_Local_Educacion')
     
 
 admin.site.register(Encargado,EncargadoAdmin)
+admin.site.register(Supervisor,SupervisorAdmin)
