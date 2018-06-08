e1c542c4893f630f71ec0b14c101048463165445
Unknown <pedro.ignaciom95@gmail.com>
2018-06-08 02:50:02 -0400

    feat: Se oculta modelo grupos de autenticacion
    se oculta por su no utilización y porque se llama de la misma forma que otro modelo, lo que puede ocasionar confusión.

--- autenticacion/admin.py
+++ autenticacion/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Usuario
+from django.contrib.auth.models import Group
+# from social_django.models import Association, Nonce, UserSocialAuth
 
 
 class UsuarioAdmin(admin.ModelAdmin):
@@ -9,7 +11,12 @@ class UsuarioAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Banear usuario', {'fields': ['is_active']}),
     ]
-
-
 admin.site.register(Usuario, UsuarioAdmin)
+
+#oculta estos modelos
+admin.site.unregister(Group)
+
+# admin.site.unregister(Association)
+# admin.site.unregister(Nonce)
+# admin.site.unregister(UserSocialAuth)
 
