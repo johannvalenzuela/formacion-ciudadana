2f28672b125f8752670766e1b4d97f640115f926
Unknown <pedro.ignaciom95@gmail.com>
2018-06-07 20:36:38 -0400

    fix: Sacada libreria no utilizada

--- biblioteca_digital/urls.py
+++ biblioteca_digital/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-from autenticacion.decorators import funcionario_required
+
 
 
 urlpatterns = [
