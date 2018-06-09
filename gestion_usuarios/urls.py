35477c7f3d4a0a2f6db998c6b99056e9362676b1
Unknown <pedro.ignaciom95@gmail.com>
2018-06-09 01:50:57 -0400

    fix: Cambiado el url de apoderado a grupos

--- gestion_usuarios/urls.py
+++ gestion_usuarios/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('alumnos/', views.ListaAlumnosView.as_view(), name='alumnos'),
     path('apoderados/', views.ListaApoderadosView.as_view(), name='apoderados'),
-    path('apoderados/', views.ListaGruposView.as_view(), name='grupos'),
+    path('grupos/', views.ListaGruposView.as_view(), name='grupos'),
 ]
