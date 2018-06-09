80569c7414186ce489cc637945e88ff5bf0416f5
Unknown <pedro.ignaciom95@gmail.com>
2018-06-09 17:03:09 -0400

    feat: Ingreso d urls ingresar/eliminar relación de usuario con grupo

--- gestion_usuarios/urls.py
+++ gestion_usuarios/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('alumnos/', views.ListaAlumnosView.as_view(), name='alumnos'),
     path('apoderados/', views.ListaApoderadosView.as_view(), name='apoderados'),
     path('grupos/', views.ListaGruposView.as_view(), name='grupos'),
+    path('agregar_usuario_grupo', views.agregarUsuario, name='agregar_usuario'),
+    path('remover_usuario_grupo', views.removerUsuario, name='remover_usuario'),
 ]
