99bd43c0e46dc0bc2c1a815f9d59163e43a10851
Unknown <pedro.ignaciom95@gmail.com>
2018-06-09 21:06:22 -0400

    fix: Arreglado nombre de url iguales

--- gestion_usuarios/urls.py
+++ gestion_usuarios/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('grupos/', views.ListaGruposView.as_view(), name='lista_grupos'),
     path('agregar_usuario_form/<int:pk_grupo>', views.agregarUsuarioFormView, name='agregar_usuario_form'),
     path('agregar_usuario_grupo/<int:pk_grupo>', views.agregarUsuario, name='agregar_usuario'),
+    path('remover_usuario_grupo_form/<int:pk_grupo>/<int:pk_usuario>', views.removerUsuarioView, name='remover_usuario_form'),
     path('remover_usuario_grupo/<int:pk_grupo>/<int:pk_usuario>', views.removerUsuario, name='remover_usuario'),
 ]
