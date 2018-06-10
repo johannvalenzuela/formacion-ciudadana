572859357d4fc88fae729ec707e8812029a8abd0
Unknown <pedro.ignaciom95@gmail.com>
2018-06-09 20:21:10 -0400

    feat: View de agregar usuario a grupo

--- gestion_usuarios/views.py
+++ gestion_usuarios/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import render 
+from django.shortcuts import redirect
 
 #decorators
 from django.contrib.auth.decorators import login_required
@@ -8,7 +9,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test
 
 #Modelos
-from .models import Grupo, Encargado
+from .models import Grupo, Encargado,RutAutorizados
 from autenticacion.models import Usuario
 
 def funcionario_required(usuario):
@@ -28,7 +29,7 @@ class ListaUsuariosView(generic.ListView):
         grupo = Grupo.objects.get(nombre=nombreGrupo, autor=autor)
         
         grupoFuncion = Grupo.objects.filter(nombre=nombreGrupo, autor=autor)
-        usuarios = Usuario.objects.filter(grupo__in=grupoFuncion)
+        usuarios = RutAutorizados.objects.filter(grupo__in=grupoFuncion)
         args = {
             "grupo": grupo,
             "lista_usuarios": usuarios,
@@ -51,25 +52,39 @@ class ListaGruposView(generic.ListView):
         autor = Encargado.objects.get(usuario=self.request.user)
         return Grupo.objects.filter(autor=autor)
 
+
 @login_required
 @user_passes_test(funcionario_required)
-def agregarUsuario(request):
+def agregarUsuarioFormView(request, pk_grupo):
+    '''
+    Muestra el formulario para agregar un usuario a un grupo en especifico
+    '''
+    grupo = Grupo.objects.get(pk=pk_grupo)
+    args = {
+        "grupo":grupo,
+    }
+    return render(request, "gestion_usuarios/usuario_add_form.html", args)
+
+
+@login_required
+@user_passes_test(funcionario_required)
+def agregarUsuario(request, pk_grupo):
     '''
     agrega una relacion nueva de muchos es a muchos de un usuario con un grupo
     '''
     if request.method =="POST":
-        grupoAIngresar = request.method.POST.get('grupo')
-        usuarioAIngresar = request.method.POST.get('usuario')
-        
-        grupo = Grupo.objects.get(nombre=grupoAIngresar, autor=request.user)
-        usuario = Usuario.objects.get(nombre=usuarioAIngresar, autor=request.user)
-        
-        usuario.grupo.add(grupoAIngresar)
-        usuario.save()
-    return redirect(grupoAIngresar)
+        nombreUsuario = request.POST.get("nombre")
+        rutUsuario = request.POST.get("rut")
+        grupo = Grupo.objects.get(pk=pk_grupo)
+        usuarioNuevo = RutAutorizados.objects.create(rut=rutUsuario,nombre=nombreUsuario)
 
+        usuarioNuevo.grupo.add(grupo)
+        usuarioNuevo.save()
+    return redirect('lista_usuarios', nombreGrupo=grupo.nombre)
+
+@login_required
 @user_passes_test(funcionario_required)
-def removerUsuario(request):
+def removerUsuario(self, request, pk_grupo, pk_user):
     '''
     elimina una relacion de muchos es a muchos de un usuario con un grupo
     '''
