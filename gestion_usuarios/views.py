faebc86ea3e158d06eb3c39ba93521e16bb25709
Unknown <pedro.ignaciom95@gmail.com>
2018-06-09 21:06:09 -0400

    feat: Agregado view para eliminar usuario de grupo

--- gestion_usuarios/views.py
+++ gestion_usuarios/views.py
@@ -2,6 +2,8 @@ from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import render 
 from django.shortcuts import redirect
+from django.core.exceptions import ObjectDoesNotExist
+
 
 #decorators
 from django.contrib.auth.decorators import login_required
@@ -76,26 +78,40 @@ def agregarUsuario(request, pk_grupo):
         nombreUsuario = request.POST.get("nombre")
         rutUsuario = request.POST.get("rut")
         grupo = Grupo.objects.get(pk=pk_grupo)
-        usuarioNuevo = RutAutorizados.objects.create(rut=rutUsuario,nombre=nombreUsuario)
-
+        try:
+            usuarioNuevo= RutAutorizados.objects.get(rut=rutUsuario)
+        except ObjectDoesNotExist:
+            usuarioNuevo = RutAutorizados.objects.create(rut=rutUsuario,nombre=nombreUsuario)
+        
         usuarioNuevo.grupo.add(grupo)
         usuarioNuevo.save()
     return redirect('lista_usuarios', nombreGrupo=grupo.nombre)
 
 @login_required
 @user_passes_test(funcionario_required)
-def removerUsuario(self, request, pk_grupo, pk_user):
+def removerUsuarioView(request, pk_grupo, pk_usuario):
+    '''
+    Muestra el formulario para eliminar un usuario de un grupo en especifico
+    '''
+    grupo = Grupo.objects.get(pk=pk_grupo)
+    usuario = RutAutorizados.objects.get(pk=pk_usuario)
+    args = {
+        "grupo":grupo,
+        "usuario":usuario,
+    }
+    return render(request, "gestion_usuarios/usuarioGrupo_delete_form.html", args)
+
+@login_required
+@user_passes_test(funcionario_required)
+def removerUsuario(request, pk_grupo, pk_usuario):
     '''
     elimina una relacion de muchos es a muchos de un usuario con un grupo
     '''
     if request.method =="POST":
-        grupoAIngresar = request.method.POST.get('grupo')
-        usuarioAIngresar = request.method.POST.get('usuario')
-        
-        grupo = Grupo.objects.get(nombre=grupoAIngresar, autor=request.user)
-        usuario = Usuario.objects.get(nombre=usuarioAIngresar, autor=request.user)
-        
-        usuario.grupo.remove(grupoAIngresar)
-        usuario.save()
-    return redirect(grupoAIngresar)
+        grupo = Grupo.objects.get(pk=pk_grupo)
+        usuarioEliminar = RutAutorizados.objects.get(pk=pk_usuario)
+            
+        usuarioEliminar.grupo.remove(grupo)
+        usuarioEliminar.save()
+    return redirect('lista_usuarios', nombreGrupo=grupo.nombre)
 
