68c4ec34c7a6a44d438658acacb4633e16305c10
Unknown <pedro.ignaciom95@gmail.com>
2018-06-11 01:10:34 -0400

    feat: Views de editar/agregar/eliminar Grupo

--- gestion_usuarios/views.py
+++ gestion_usuarios/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.shortcuts import render 
 from django.shortcuts import redirect
 from django.core.exceptions import ObjectDoesNotExist
-
+from django.http import HttpResponseRedirect
 
 #decorators
 from django.contrib.auth.decorators import login_required
@@ -13,6 +13,7 @@ from django.contrib.auth.decorators import user_passes_test
 #Modelos
 from .models import Grupo, Encargado,RutAutorizados
 from autenticacion.models import Usuario
+
 
 def funcionario_required(usuario):
     return Encargado.objects.get(usuario=usuario)
@@ -114,4 +115,78 @@ def removerUsuario(request, pk_grupo, pk_usuario):
         usuarioEliminar.grupo.remove(grupo)
         usuarioEliminar.save()
     return redirect('lista_usuarios', nombreGrupo=grupo.nombre)
+
+
+@method_decorator(login_required, name='get' )
+@method_decorator(user_passes_test(funcionario_required), name='get')
+class AgregarGrupoView(generic.CreateView):
+    '''
+    Es la clase para agregar un grupo de un encargado en especifico.
+    '''
+    model = Grupo
+    fields = ['nombre']
+    template_name = 'gestion_usuarios/grupo_agregar_form.html'
+    success_url = reverse_lazy('lista_grupos')
+
+    def form_valid(self, form):
+        '''
+        Esta funcion se tira si el formulario es valido
+        '''
+        autor = Encargado.objects.get(usuario=self.request.user)
+        establecimiento = autor.establecimiento
+
+        form.instance.autor = autor
+        form.instance.establecimiento = establecimiento
+        return super().form_valid(form)
+
+    # def form_valid(self, form):
+    #     grupo = form.save(commit=False)
+    #     autorNuevo = Encargado.objects.get(usuario=self.request.user)
+    #     establecimientoNuevo = autorNuevo.establecimiento
+
+    #     grupo.autor = autorNuevo
+    #     grupo.establecimiento = establecimientoNuevo
+    #     return super(AgregarGrupoView, self).form_valid(form)
+
+
+
+@method_decorator(login_required, name='get' )
+@method_decorator(user_passes_test(funcionario_required), name='get')
+class EditarGrupoView(generic.UpdateView):
+    '''
+    Es la clase para editar un grupo de un encargado en especifico.
+    '''
+    model = Grupo
+    fields = ['nombre',]
+    template_name_suffix = '_editar_form'
+    
+    def get_object(self, queryset=None):
+        '''
+        Esta función es para obtener el objeto a editar
+        Se creo para que no funcionara si el grupo es alumnos o apoderados
+        EN PRUEBA(POSIBLEMENTE TENGO BUGS)
+        '''
+        obj = Grupo.objects.get(pk=self.kwargs['pk'])
+        if obj.nombre in "alumnos" or obj.nombre in "apoderados":
+            return None
+        return obj
+
+    def get_success_url(self):
+        '''
+        Es la función que se lanza una vez se realizo todo de forma correcta
+        '''
+	    return reverse_lazy('lista_usuarios', kwargs={'nombreGrupo': self.object.nombre})
+
+@method_decorator(login_required, name='get' )
+@method_decorator(user_passes_test(funcionario_required), name='get')
+class EliminarGrupoView(generic.DeleteView):
+    '''
+    Es la clase para eliminar un grupo de un encargado en especifico.
+    '''
+    model = Grupo
+    template_name_suffix = '_eliminar_form'
+    success_url = reverse_lazy('lista_grupos')
+
+
+
 
