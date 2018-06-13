7276b24fbd120747b430ff25caa70d83077809d8
Unknown <pedro.ignaciom95@gmail.com>
2018-06-13 09:50:33 -0400

    feat: Agregadas las urls nuevas de las vistas genericas

--- gestion_usuarios/urls.py
+++ gestion_usuarios/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('grupos/agregar_grupo/', views.AgregarGrupoView.as_view(), name='agregar_grupo'),
     path('grupos/eliminar_grupo/<int:pk>/', views.EliminarGrupoView.as_view(), name='eliminar_grupo'),
     path('grupos/editar_grupo/<int:pk>/', views.EditarGrupoView.as_view(), name='editar_grupo'),
-    path('agregar_usuario_form/<int:pk_grupo>', views.agregarUsuarioFormView, name='agregar_usuario_form'),
-    path('agregar_usuario_grupo/<int:pk_grupo>', views.agregarUsuario, name='agregar_usuario'),
-    path('remover_usuario_grupo_form/<int:pk_grupo>/<int:pk_usuario>', views.removerUsuarioView, name='remover_usuario_form'),
-    path('remover_usuario_grupo/<int:pk_grupo>/<int:pk_usuario>', views.removerUsuario, name='remover_usuario'),
+    path('agregar_usuario_form/<int:pk_grupo>', views.AgregarUsuarioGrupoView.as_view(), name='agregar_usuario_form'),
+    path('remover_usuario_grupo_form/<int:pk_grupo>/<int:pk_usuario>', views.EliminarUsuarioGrupoView.as_view(), name='remover_usuario_form'),
 ]
