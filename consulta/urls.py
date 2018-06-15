ddc8b3cb14a4ad53c143c8e19222d556cfd51573
Pedro <pedro.ignaciom95@gmail.com>
2018-06-15 19:00:58 -0400

    feat: url para el CRUD de propuestas de consulta

--- consulta/urls.py
+++ consulta/urls.py
@@ -3,11 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.VisualizarConsultasView.as_view(), name='visualizar_consultas'),
-    path('<int:pk>/', views.DetallesConsultaView.as_view(), name='detalles_consulta'),
+    path('detallesconsulta/<int:pk>/', views.DetallesConsultaView.as_view(), name='detalles_consulta'),
     path('crearconsulta/', views.CrearConsultaView.as_view(), name = 'crear_consulta'),
-    path('detallesconsulta/crearpropuestaconsulta/', views.ConsultaCrearPropuestaView.as_view(), name ='crear_propuesta'),
-    path('detallesconsulta/responderconsulta/', views.ResponderConsultaView.as_view(), name= 'responder_consulta'),
-    path('visualizarpropuestaconsulta/', views.ConsultaVisualizarPropuestaView.as_view(), name= 'propuesta_consulta'),
+    path('detallesconsulta/<int:pk>/responderconsulta/', views.ResponderConsultaView.as_view(), name= 'responder_consulta'),
     path('<int:pk>/eliminar', views.ConsultaDeleteView.as_view(), name='consulta_delete'),
     path('<int:pk>/editar', views.ConsultaUpdateView.as_view(), name='consulta_update'),
+
+    #crear, modificar, visualizar y eliminar propuestas de consulta
+    path('detallesconsulta/<int:pk>/crearpropuestaconsulta/', views.PropuestaConsultaCreateView.as_view(), name ='crear_propuesta'),
+    path('visualizarpropuesta/<int:pk>/', views.PropuestaConsultaVisualizarView.as_view(), name= 'visualizar_propuesta'),
+    path('visualizarpropuesta/<int:pk>/editar', views.PropuestaConsultaUpdateView.as_view(), name ='editar_propuesta'),
+    path('visualizarpropuesta/<int:pk>/eliminar', views.PropuestaConsultaDeleteView.as_view(), name ='eliminar_propuesta'),
 ]
