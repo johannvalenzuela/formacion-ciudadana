9bd2d287c7545bea042ebb316997f569daa8d2bb
Pedro <pedro.ignaciom95@gmail.com>
2018-06-16 20:35:19 -0400

    feat: Cambiado el nombre de la url de la votacion

--- consulta/urls.py
+++ consulta/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.VisualizarConsultasView.as_view(), name='visualizar_consultas'),
     path('detallesconsulta/<int:pk>/', views.DetallesConsultaView.as_view(), name='detalles_consulta'),
     path('crearconsulta/', views.CrearConsultaView.as_view(), name = 'crear_consulta'),
-    path('detallesconsulta/<int:pk>/responderconsulta/', views.ResponderConsultaView.as_view(), name= 'responder_consulta'),
+    path('detallesconsulta/<int:pk>/votar/', views.ResponderConsultaView.as_view(), name= 'consulta_votar'),
     path('detallesconsulta/<int:pk>/eliminar', views.ConsultaDeleteView.as_view(), name='consulta_delete'),
     path('detallesconsulta/<int:pk>/editar', views.ConsultaUpdateView.as_view(), name='consulta_update'),
 
