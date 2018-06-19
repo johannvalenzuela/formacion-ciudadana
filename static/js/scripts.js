f093f8aa77955e393736f797289bb5438c95bf8b
Israel Ogas <ogasvega.israel@gmail.com>
2018-06-19 15:05:54 -0400

    feat: añadido script para seleccionar grupos sin CTRL o SHIFT

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -101,3 +101,10 @@ $(document).ready(function () {
         });
     });
 });
+
+
+$('option').mousedown(function(e) {
+    e.preventDefault();
+    $(this).prop('selected', !$(this).prop('selected'));
+    return false;
+});
