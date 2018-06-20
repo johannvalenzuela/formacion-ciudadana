40c189a916769f1b58c1548a827c58553a4d04f3
Pedro <pedro.ignaciom95@gmail.com>
2018-06-20 17:48:23 -0400

    fix: Agregadas importaciones faltantes

--- analitica/models.py
+++ analitica/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from datetime import datetime
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
-#modelos importados
-from gestion_usuarios import Asignatura, Departamento_Provincial_Educacion, Servicio_Local_Educacion, Encargado
+#modelos
+from gestion_usuarios.models import Asignatura, Departamento_Provincial_Educacion, Servicio_Local_Educacion, Encargado
+
+
 
 class Supervisor(models.Model):
-    asignatura = models.ForeignKey('Asignatura', on_delete = models.CASCADE)
-    departamento_Provincial_Educacion = models.ForeignKey('Departamento_Provincial_Educacion', on_delete = models.CASCADE)
-    servicio_Local_Educacion = models.ForeignKey('Servicio_Local_Educacion', on_delete = models.CASCADE)
+    asignatura = models.ForeignKey(Asignatura, on_delete = models.CASCADE)
+    departamento_Provincial_Educacion = models.ForeignKey(Departamento_Provincial_Educacion, on_delete = models.CASCADE)
+    servicio_Local_Educacion = models.ForeignKey(Servicio_Local_Educacion, on_delete = models.CASCADE)
     usuario = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
 
     def __str__(self):
@@ -25,5 +29,5 @@ class Actividad(models.Model):
     titulo = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=300)
     link = models.URLField(max_length=200)
-    encargado = models.ForeignKey('Encargado',on_delete=models.CASCADE)
+    encargado = models.ForeignKey(Encargado,on_delete=models.CASCADE)
     fecha = models.DateField("%d-%m-%Y", default=datetime.now)
