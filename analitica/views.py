56897c650839de459713b4d22f0af7400f64af50
Pedro <pedro.ignaciom95@gmail.com>
2018-06-20 18:00:22 -0400

    feat: Agregado decorator especifico para las vistas del supervisor

--- analitica/views.py
+++ analitica/views.py
@@ -2,19 +2,34 @@ from django.views import generic
 from django.urls import reverse_lazy
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
+from django.shortcuts import redirect
 
+#decorators
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import user_passes_test
 
 #modelos
 from .models import Supervisor, Actividad
 from gestion_usuarios.models import Encargado
 
+def supervisor_required(user):
+    try:
+        supervisor = Supervisor.objects.get(usuario=user)
+    except ObjectDoesNotExist:
+        return None
+    
+    return supervisor
+
+@method_decorator(login_required, name='get' )
+@method_decorator(user_passes_test(supervisor_required), name='get')
 class ActividadesListView(generic.ListView):
     '''
     Muestra la lista de actividades que puede ver el supervisor
     '''
     model= Actividad
     context_object_name = 'actividades'
-    template_name = 'gestion_usuarios/actividades_lista.html'
+    template_name = 'analitica/actividades_lista.html'
     success_url = reverse_lazy('lista_actividades')
 
     def get_queryset(self):
@@ -26,7 +41,9 @@ class ActividadesListView(generic.ListView):
         else:
             supervisados = Encargado.objects.filter(supervisor=supervisor)
             return Actividad.objects.filter(encargado__in=supervisados)
-            
+
+@method_decorator(login_required, name='get' )
+@method_decorator(user_passes_test(supervisor_required), name='get')           
 class AsociarEncargadoView(generic.ListView):
     """
     Muestra la lista de los encargados que puede agregar un supervisor para ser
@@ -35,7 +52,7 @@ class AsociarEncargadoView(generic.ListView):
     """
     model = Encargado
     context_object_name = 'encargados'
-    template_name = 'gestion_usuarios/asociar_encargado.html'
+    template_name = 'analitica/asociar_encargado.html'
     success_url = reverse_lazy('lista_actividades')
 
     def get_queryset(self):
