159297bd7a69ca053c1a9ba979c02227f8a69ac0
Pedro <pedro.ignaciom95@gmail.com>
2018-06-22 11:21:23 -0400

    fix: Cambiado el campo de autor de consulta a Encargado

--- consulta/models.py
+++ consulta/models.py
@@ -3,11 +3,14 @@ from datetime import datetime
 from gestion_usuarios.models import Grupo
 from django.conf import settings
 
+#modelos 
+from gestion_usuarios.models import Encargado
+
 class Consulta(models.Model): 
     ''' 
     Es la consulta 
     '''
-    autor = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    autor = models.ForeignKey(Encargado,on_delete=models.CASCADE)
     titulo = models.CharField(max_length = 100) 
     fecha_creacion = models.DateField("%d-%m-%Y", default= datetime.now) 
     fecha_finalizacion = models.DateField("%d-%m-%Y", null=False) 
