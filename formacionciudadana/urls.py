93337c259e5c82bbc804a95313acb008212bfb08
Pedro <pedro.ignaciom95@gmail.com>
2018-06-24 13:44:14 -0400

    feat: Cambio de default url para el admin
    Se agrega una url nueva al admin por seguridad.

--- formacionciudadana/urls.py
+++ formacionciudadana/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin_iipj/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('autenticacion/', include('autenticacion.urls')),
     path('biblioteca_digital/', include('biblioteca_digital.urls')),
