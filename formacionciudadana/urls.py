bb566ad613f0849df539336a89798ce170194009
Israel Ogas <ogasvega.israel@gmail.com>
2018-06-24 23:15:44 -0400

    fix: mostrar imagen de perfil en navbar

--- formacionciudadana/urls.py
+++ formacionciudadana/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin_iipj/', admin.site.urls),
@@ -12,3 +14,6 @@ urlpatterns = [
     path('consulta/', include('consulta.urls')),
     path('analitica', include('analitica.urls')),
 ]
+if  settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
