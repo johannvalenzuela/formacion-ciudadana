6b02759bcb20510ab4109bf6a5d5da9dfc2502ab
Pedro <pedro.ignaciom95@gmail.com>
2018-06-26 11:27:15 -0400

    feat: Creado validator para el tipo de archivo subido por el usuario

--- biblioteca_digital/validators.py
+++ biblioteca_digital/validators.py
@@ -6,3 +6,10 @@ def file_size(value): # add this to some file where you can import it from
     if value.size > limit:
         raise ValidationError('Archivo demasiado pesado, no debe pasar los 5MB.')
 
+def pdf_type(value):
+    pdf_type= "application/pdf"
+
+    if value.content_type==pdf_type:
+        pass
+    else:
+        raise ValidationError('El archivo debe ser de tipo pdf.')
