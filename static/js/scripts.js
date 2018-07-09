0710abfb15a2bd72c48085a69d48ed2e8981f34e
Israel Ogas <ogasvega.israel@gmail.com>
2018-07-09 18:13:05 -0400

    fix: cambio de id en la funcion

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -46,7 +46,7 @@ SetRatingStar();
 
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
-        $('#sidebar, #content').toggleClass('active');
+        $('#side-nav, #content').toggleClass('active');
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
